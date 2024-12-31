98da34498f3cad0f2443eb8ba13cef46aefba857
ook3d <47336113+ook3D@users.noreply.github.com>
2024-12-31 11:00:17 -0500

    fix radio station patch crashing rockstar editor

--- WorldTravelPatches/src/RadioStations.h
+++ WorldTravelPatches/src/RadioStations.h
@@ -13,31 +13,31 @@ public:
 		uint8_t* maxRadioStations = hook::get_pattern<uint8_t>("8D 5E 60 89 35", 2); // audRadioStation::InitClass
 		DWORD oldProtect;
 		VirtualProtect(maxRadioStations, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*maxRadioStations = 255;
+		*maxRadioStations = 127;
 		VirtualProtect(maxRadioStations, 1, oldProtect, &oldProtect);
 		uint32_t* radioStationsMemSize = (uint32_t*)(maxRadioStations + 0x26);
 		VirtualProtect(radioStationsMemSize, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*radioStationsMemSize = 255 * 8;
+		*radioStationsMemSize = 127 * 8;
 		VirtualProtect(radioStationsMemSize, 4, oldProtect, &oldProtect);
 
 
-		uint8_t* maxRadioStations2 = hook::get_pattern<uint8_t>("BD 60 00 00 00", 1);
+		uint8_t* maxRadioStations2 = hook::get_pattern<uint8_t>("BD 60 00 00 00 E9", 1);
 		VirtualProtect(maxRadioStations2, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*maxRadioStations2 = 255;
+		*maxRadioStations2 = 127;
 		VirtualProtect(maxRadioStations2, 1, oldProtect, &oldProtect);
 		uint32_t* radioStationsMemSize2 = (uint32_t*)(maxRadioStations2 + 0x43);
 		VirtualProtect(radioStationsMemSize2, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*radioStationsMemSize2 = 255 * 8;
+		*radioStationsMemSize2 = 127 * 8;
 		VirtualProtect(radioStationsMemSize2, 4, oldProtect, &oldProtect);
 
 
-		uint8_t* maxRadioStations3 = hook::get_pattern<uint8_t>("45 8D 67 ? 8B C6", 2);
+		uint8_t* maxRadioStations3 = hook::get_pattern<uint8_t>("45 8D 67 ? 8B C6", 3); // audRadioStation::RemoveRadioStationList
 		VirtualProtect(maxRadioStations3, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*maxRadioStations3 = 255;
+		*maxRadioStations3 = 127;
 		VirtualProtect(maxRadioStations3, 1, oldProtect, &oldProtect);
-		uint32_t* radioStationsMemSize3 = (uint32_t*)(maxRadioStations3 + 0x143);
+		uint32_t* radioStationsMemSize3 = (uint32_t*)(maxRadioStations3 + 0x141);
 		VirtualProtect(radioStationsMemSize3, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*radioStationsMemSize3 = 255 * 8;
+		*radioStationsMemSize3 = 127 * 8;
 		VirtualProtect(radioStationsMemSize3, 4, oldProtect, &oldProtect);
 	}
 };
